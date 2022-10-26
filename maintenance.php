73b36c870721877abeed2cdb16ce9897de4f3534
NuggetReckt <corto.morrow@gmail.com>
2022-10-26 21:32:26 +0200

    for sync

--- maintenance.php
+++ maintenance.php
@@ -8,11 +8,11 @@
     <meta name="author" content="NuggetReckt">
     <meta name="keywords"
           content="serv, mc, minecraft, Minecraft, aventure, java, 1.18, 1.18.2, fr, FR, français, french, génération custom, serveur, premium, survie, créatif, crea, NoSkillWorld, NuggetReckt, Herbyvor, nsw, Semi-RP, events, hardcore">
-    <link rel="apple-touch-icon" sizes="180x180" href="assets/images/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon-16x16.png">
-    <link rel="manifest" href="assets/images/site.webmanifest">
-    <link rel="mask-icon" href="assets/images/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?="assets/images/apple-touch-icon.png"?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?="assets/images/favicon-32x32.png"?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?="assets/images/favicon-16x16.png"?>">
+    <link rel="manifest" href="<?="assets/images/site.webmanifest"?>">
+    <link rel="mask-icon" href="<?="assets/images/safari-pinned-tab.svg"?>" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#2C70BA">
 
@@ -33,7 +33,7 @@
     <meta name="twitter:description"
           content="Site internet du serveur Minecraft NoSkillWorld. Rejoins le serveur avec tes amis ou solo pour vivre de formidables aventures !">
 
-    <link rel="stylesheet" href="assets/css/maintenance.css">
+    <link rel="stylesheet" href="<?="assets/css/maintenance.css"?>">
 </head>
 <body>
 <div class="maintenance-content">
