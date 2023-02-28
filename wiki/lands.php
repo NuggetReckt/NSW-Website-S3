5608bf875ae0b5a1e084cc4127883aeba575252a
NuggetReckt <corto.morrow@gmail.com>
2023-02-28 03:24:49 +0100

    Added: Title, and home link for wiki pages Started: Wiki contents styles

--- wiki/lands.php
+++ wiki/lands.php
@@ -6,7 +6,20 @@ $pager = new WikiPager("Lands");
 $pager->setHeader();
 ?>
 
+    <div class="top-content top-content-others" id="top-content-wiki">
+        <div class="top-wiki-nav">
+            <h1>Les Lands</h1>
+            <a href="/wiki">Revenir au Menu</a>
+        </div>
+    </div>
+    <div class="wiki-content">
+        <div class="table-of-contents-wiki">
 
+        </div>
+        <div class="wiki-article">
+
+        </div>
+    </div>
 
 <?php
 $pager->setFooter();
--- wiki/lore.php
+++ wiki/lore.php
@@ -6,7 +6,20 @@ $pager = new WikiPager("Lore");
 $pager->setHeader();
 ?>
 
+    <div class="top-content top-content-others" id="top-content-wiki">
+        <div class="top-wiki-nav">
+            <h1>Le Lore</h1>
+            <a href="/wiki">Revenir au Menu</a>
+        </div>
+    </div>
+    <div class="wiki-content">
+        <div class="table-of-contents-wiki">
 
+        </div>
+        <div class="wiki-article">
+
+        </div>
+    </div>
 
 <?php
 $pager->setFooter();
