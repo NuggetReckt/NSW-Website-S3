676f3c380090764194186eaa8161cf6d3edced02
NuggetReckt <corto.morrow@gmail.com>
2023-04-11 20:06:56 +0200

    Added: Mumble wiki, not finished, for sync

--- wiki/mumble.php
+++ wiki/mumble.php
@@ -23,7 +23,10 @@ $pager->setHeader();
                     <a href="#2">Prérequis</a>
                 </li>
                 <li class="table-wiki-item">
-                    <a href="#3">Installation / Configuration</a>
+                    <a href="#3">Installation</a>
+                </li>
+                <li class="table-wiki-item">
+                    <a href="#3">Configuration</a>
                 </li>
             </ol>
         </div>
@@ -49,7 +52,7 @@ $pager->setHeader();
                     <p class="wiki-p wiki-p-no-indent">
                         - Le logiciel Mumble<br>
                         - Une connexion internet<br>
-                        - Avoir les accès d'administrateur
+                        - Avoir les accès d'administrateur sur son ordinateur
                     </p>
                 </div>
                 <div class="wiki-article-content">
@@ -64,27 +67,89 @@ $pager->setHeader();
                         - Le mod MumbleLink pour Fabric
                     </p>
                 </div>
-                <div class="wiki-article-content">
-                </div>
             </div>
             <div class="wiki-article-title">
-                <h2 id="3">3. Installation / Configuration</h2>
+                <h2 id="3">3. Installation</h2>
                 <hr>
             </div>
             <div class="wiki-article-wrapper">
-                <div class="wiki-article-content-centered">
-                    <div class="wiki-subsection">
-                        <img src="../assets/images/background.png" class="wiki-img-exemple" alt="gui1-img">
-                        <p class="wiki-p-img-legend">Image 1</p>
-                    </div>
-                    <div class="wiki-subsection">
-                        <img src="../assets/images/background.png" class="wiki-img-exemple" alt="gui2-img">
-                        <p class="wiki-p-img-legend">Image 2</p>
-                    </div>
+                <div class="wiki-article-content">
+                    <img src="../assets/images/information-point.png" class="wiki-img-highlight wiki-img-highlight-info"
+                         alt="info">
+                    <p class="wiki-p-highlight">
+                        Si tu utilises un client custom comprenant le MumbleLink, tu peux passer les étapes 3️⃣, 4️⃣ et
+                        5️⃣
+                    </p>
+                </div>
+                <div class="wiki-article-content">
+                    <img src="../assets/images/caution.png" class="wiki-img-highlight wiki-img-highlight-warn"
+                         alt="warn">
+                    <p class="wiki-p-highlight">
+                        La version Mac de mumble actuelle n'est pas stable.<br>
+                        Il est possible que certaines fonctionnalités ne fonctionne pas pour le moment.
+                    </p>
+                </div>
+                <div class="wiki-article-content-steps">
+                    <img src="../assets/images/step1.png" class="wiki-img-step"
+                         alt="step">
+                    <p class="wiki-p-step">
+                        Installe le client Mumble depuis le lien ci-dessous selon ton système d'exploitation :<br>
+                        - <a href="https://download.mumble.com/en/mumble_client-1.4.230.x64.msi"
+                             target="_blank">Windows</a><br>
+                        - <a href="https://download.mumble.com/en/mumble_client-1.4.230.x64.dmg"
+                             target="_blank">MacOs</a>
+                    </p>
+                </div>
+                <div class="wiki-article-content-steps">
+                    <img src="../assets/images/step2.png" class="wiki-img-step"
+                         alt="step">
+                    <p class="wiki-p-step">
+                        Exécute le fichier précédemment enregistré, et suis les étapes d'installation.<br>
+                        C'est fait ! Ne lance pas Mumble de suite.
+                    </p>
+                </div>
+                <div class="wiki-article-content-steps">
+                    <img src="../assets/images/step3.png" class="wiki-img-step"
+                         alt="step">
+                    <p class="wiki-p-step">
+                        Télécharge l'installeur pour Fabric en cliquant sur le lien suivant :<br>
+                        <a href="https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.11.2/fabric-installer-0.11.2.jar"
+                           target="_blank">Télécharger Fabric</a>
+                    </p>
+                </div>
+                <div class="wiki-article-content-steps">
+                    <img src="../assets/images/step4.png" class="wiki-img-step"
+                         alt="step">
+                    <p class="wiki-p-step">
+                        Ouvre le fichier <span class="wiki-command-exemple">fabric-installer-x.x.x.jar</span> precedent
+                        téléchargé et une fois la fenêtre ouverte, sélectionne la
+                        version [version], laisse la rubrique "Version du loader" tel quel, veille à ce que "Créer un
+                        profil" soit coché et clique sur "Installer". Fabric va s'installer et une fois fini, il devrait
+                        apparaître dans le launcher de Minecraft.<br>
+                        S'il n'apparait pas, pas de panique ! Dans le launcher
+                        officiel, clique sur l'onglet "Configuration" puis clique sur "Nouvelle configuration". Ensuite
+                        cherche la bonne version de Fabric et créé ta nouvelle configuration
+                    </p>
+                </div>
+                <div class="wiki-article-content-steps">
+                    <img src="../assets/images/step3.png" class="wiki-img-step"
+                         alt="step">
+                    <p class="wiki-p-step">
+                        Télécharge le mod MumbleLink en cliquant sur le lien suivant (Toujours pour la version [version]
+                        de Minecraft) :<br>
+                        <a href="" target="_blank">Télécharger le MumbleLink</a> <br>
+                        Puis place le fichier <span class="wiki-command-exemple">fabric-mumblelink-mod-x.x.x.jar</span>
+                        dans le dossier <span class="wiki-command-exemple">mods</span> dans ton <span
+                                class="wiki-command-exemple">.minecraft</span>. <br><br>
+                        Comment accéder au dossier <span class="wiki-command-exemple">.minecraft</span> ?<br>
+                        Execute le raccourci clavier <span class="wiki-command-exemple">WIN + R</span> et tape l'entrée
+                        <span class="wiki-command-exemple">%appdata%</span> dans la fenêtre qui vient de s'ouvrir. Tu
+                        trouveras dans ce dossier le fameux <span class="wiki-command-exemple">.minecraft</span>.
+                    </p>
                 </div>
             </div>
             <div class="wiki-article-title">
-                <h2 id="4">4. </h2>
+                <h2 id="4">4. Configuration</h2>
                 <hr>
             </div>
             <div class="wiki-article-wrapper">
