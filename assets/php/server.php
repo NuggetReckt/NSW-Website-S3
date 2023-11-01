6ae791ebfbd6044fa13ad9f9f1b28708f0d4e89e
NuggetReckt <84108267+NuggetReckt@users.noreply.github.com>
2023-11-01 03:05:29 +0100

    Added: Async https requests Added: Composer dependencies

--- assets/php/server.php
+++ assets/php/server.php
@@ -1,5 +1,9 @@
 <?php
 
+require __DIR__ . '/../../vendor/autoload.php';
+
+use function Amp\async;
+
 class Server
 {
     private int $players_count;
@@ -8,9 +12,12 @@ class Server
 
     public function __construct()
     {
-        $this->players_count = file_get_contents('https://www.serveurs-minecraft.org/api/players_count.php?id=60934');
-        $this->slots_count = file_get_contents('https://www.serveurs-minecraft.org/api/slots_count.php?id=60934');
-        $this->is_online = file_get_contents('https://www.serveurs-minecraft.org/api/is_online.php?id=60934');
+        $future = async(function () {
+            $this->players_count = file_get_contents('https://www.serveurs-minecraft.org/api/players_count.php?id=60934');
+            $this->slots_count = file_get_contents('https://www.serveurs-minecraft.org/api/slots_count.php?id=60934');
+            $this->is_online = file_get_contents('https://www.serveurs-minecraft.org/api/is_online.php?id=60934');
+        });
+        $future->await();
     }
 
     public function getPlayerCount(): string
@@ -24,15 +31,11 @@ class Server
 
     public function getServerStatus(): string
     {
-        switch ($this->is_online) {
-            case 1:
-                return "Serveur en ligne :)";
-            case 0:
-                return "Serveur hors ligne :/";
-            case -1:
-                return "Erreur 💩";
-            default:
-                return "Erreur inconnue";
-        }
+        return match ($this->is_online) {
+            1 => "Serveur en ligne :)",
+            0 => "Serveur hors ligne :/",
+            -1 => "Erreur 💩",
+            default => "Erreur inconnue",
+        };
     }
 }
