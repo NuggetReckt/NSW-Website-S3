907855faf1645a13599d71d20154ba80d33fa121
NuggetReckt <84108267+NuggetReckt@users.noreply.github.com>
2023-11-04 21:57:13 +0100

    Added: Meta tags in maintenance page Improved: Made logo bigger in home page

--- maintenance.php
+++ maintenance.php
@@ -2,41 +2,50 @@
 <html lang="FR-fr">
 <head>
     <title>Maintenance - NoSkillWorld</title>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="NuggetReckt">
     <meta name="keywords"
-          content="serv, mc, minecraft, Minecraft, aventure, java, 1.18, 1.18.2, fr, FR, français, french, génération custom, serveur, premium, survie, créatif, crea, NoSkillWorld, NuggetReckt, Herbyvor, nsw, Semi-RP, events, hardcore">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?="assets/images/apple-touch-icon.png"?>">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?="assets/images/favicon-32x32.png"?>">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?="assets/images/favicon-16x16.png"?>">
-    <link rel="manifest" href="<?="assets/images/site.webmanifest"?>">
-    <link rel="mask-icon" href="<?="assets/images/safari-pinned-tab.svg"?>" color="#5bbad5">
+          content="serv, mc, minecraft, Minecraft, aventure, java, 1.18, 1.18.2, 1.19, 1.19.2, 1.19.3, 1.20, Fr, fr, FR, français, french, génération custom, serveur, premium, survie, créatif, crea, NoSkillWorld, NuggetReckt, Herbyvor, nsw, Semi-RP, events, hardcore">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon-16x16.png">
+    <link rel="manifest" href="assets/images/site.webmanifest">
+    <link rel="mask-icon" href="assets/images/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
+
+    <meta name="description"
+          content="Site internet du serveur Minecraft NoSkillWorld. Rejoins le serveur avec tes amis ou en solo pour vivre de formidables aventures !">
+
+    <!-- Facebook, Discord and LinkedIn Meta Tags -->
     <meta name="theme-color" content="#2C70BA">
-
-    <meta property="og:image" content="https://play.noskillworld.fr/static/img/favicon.png">
-    <meta property="og:image:type" content="object">
-    <meta property="og:image:width" content="200">
-    <meta property="og:image:height" content="150">
-    <meta property="og:image:alt" content="logo NoSkillWorld">
-    <meta property="og:site_name" content="play.noskillworld.fr">
-    <meta property="og:title" content="Maintenancec - NoSkillWorld">
-    <meta property="og:type" content="website">
     <meta property="og:url" content="https://play.noskillworld.fr">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="play.noskillworld.fr">
+    <meta property="og:title" content="Site - NoSkillWorld">
     <meta property="og:description"
-          content="Site internet du serveur Minecraft NoSkillWorld. Rejoins le serveur avec tes amis ou solo pour vivre de formidables aventures !">
-    <meta name="twitter:site" content="play.noskillworld.fr">
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:image:src" content="https://play.noskillworld.fr/static/img/favicon.png">
-    <meta name="twitter:description"
-          content="Site internet du serveur Minecraft NoSkillWorld. Rejoins le serveur avec tes amis ou solo pour vivre de formidables aventures !">
+          content="Site internet du serveur Minecraft NoSkillWorld. Rejoins le serveur avec tes amis ou en solo pour vivre de formidables aventures !">
+    <meta property="og:image" content="https://play.noskillworld.fr/assets/images/embed-icon.png">
+    <meta property="og:image:alt" content="logo_nsw">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="50">
+    <meta property="og:image:height" content="50">
 
-    <link rel="stylesheet" href="<?="assets/css/maintenance.css"?>">
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content="play.noskillworld.fr">
+    <meta property="twitter:url" content="https://play.noskillworld.fr">
+    <meta name="twitter:title" content="Site - NoSkillWorld">
+    <meta name="twitter:description"
+          content="Site internet du serveur Minecraft NoSkillWorld. Rejoins le serveur avec tes amis ou en solo pour vivre de formidables aventures !">
+    <meta name="twitter:image" content="https://play.noskillworld.fr/assets/images/background.png">
+
+    <link rel="stylesheet" href="<?= "assets/css/maintenance.css" ?>">
 </head>
 <body>
 <div class="maintenance-content">
-    <a href="index.php">
+    <a href="https://play.noskillworld.fr/">
         <img id="logo" src="assets/images/logo_nsw.png" alt="logo">
     </a>
     <h1>Site en maintenance</h1>
