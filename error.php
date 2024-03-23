2e966d25f6a2fdce335d35b3d0ba9910269a73a1
NuggetReckt <corto.morrow@gmail.com>
2024-03-24 00:44:12 +0100

    Improved: Error code handler

--- error.php
+++ error.php
@@ -1,6 +1,6 @@
 <?php
 require_once "assets/php/pager.php";
-$pager = new Pager("Error");
+$pager = new Pager("Erreur");
 
 $pager->setHeader();
 
@@ -9,35 +9,7 @@ $err = filter_input(INPUT_GET, 'error', FILTER_VALIDATE_INT);
 
         <div class="top-content top-content-others" id="top-content-error">
             <div class="top-title">
-                <h1 id="error-title">Error:
-
-                    <?php
-                    if (isset($err)) {
-                        switch ($err) {
-                            case 403:
-                                echo $err, " No Permission.";
-                                break;
-                            case 404:
-                                echo $err, " Not Found.";
-                                break;
-                            case 500:
-                                echo $err, " Internal Server Error.";
-                                break;
-                            case 502:
-                                echo $err, " Bad Gateway.";
-                                break;
-                            case 503:
-                                echo $err, " Service Unavailable.";
-                                break;
-                            case 504:
-                                echo $err, " Gateway Timeout.";
-                                break;
-                            default:
-                                echo "Unknown error.";
-                        }
-                    }
-                    ?>
-                </h1>
+                <h1 id="error-title">Error: <?php echo get_error() ?></h1>
             </div>
             <div class="error-subtitle">
                 <a href="index.php" id="back-link">Revenir à l'accueil</a><span id="error-separator">ou</span><a href="https://discord.gg/cE4eHEZWrD" target="_blank" id="discord-link">Soumettre le bug</a>
@@ -46,4 +18,28 @@ $err = filter_input(INPUT_GET, 'error', FILTER_VALIDATE_INT);
 
 <?php
 $pager->setFooter();
+
+function get_error(): string
+{
+    if (!isset($err)) {
+        return "The error isn't set.";
+    }
+    switch ($err) {
+        case 403:
+            return "$err No Permission.";
+        case 404:
+            return "$err Not Found.";
+        case 500:
+            return "$err Internal Server Error.";
+        case 502:
+            return "$err Bad Gateway.";
+        case 503:
+            return "$err Service Unavailable.";
+        case 504:
+            return "$err Gateway Timeout.";
+        default:
+            break;
+    }
+    return "Unknown error.";
+}
 ?>
